4aaf24063c36eae4776eeefb08c78aadbf3ade1c
miche <michelleskater28@gmail.com>
2025-07-21 15:41:50 -0400

    change to <= in encoder

--- src/colour_encoder.v
+++ src/colour_encoder.v
@@ -4,19 +4,19 @@
 module colour_encoder (
     input  wire       oe, //active high enable
     input wire [1:0] colour_enc_in, 
-    output  wire [3:0] uo   
+    output  reg [3:0] uo   
 );
 
     always @(*) begin
         if (oe) begin
             // Set outputs based on input encoding
-            uo[0] = ~colour_enc_in[0] & ~colour_enc_in[1]; // red
-            uo[1] =  colour_enc_in[0] & ~colour_enc_in[1]; // blue
-            uo[2] = ~colour_enc_in[0] &  colour_enc_in[1]; // yellow
-            uo[3] =  colour_enc_in[0] &  colour_enc_in[1]; // green
+            uo[0] <= ~colour_enc_in[0] & ~colour_enc_in[1]; // red
+            uo[1] <=  colour_enc_in[0] & ~colour_enc_in[1]; // blue
+            uo[2] <= ~colour_enc_in[0] &  colour_enc_in[1]; // yellow
+            uo[3] <=  colour_enc_in[0] &  colour_enc_in[1]; // green
         end else begin
             // High-impedance when output is disabled
-            uo = 4'b0000;
+            uo <= 4'b0000;
         end
     end
 
